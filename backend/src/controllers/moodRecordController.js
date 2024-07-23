fc485759fa1b0941e6dd5d035dbe7a11c675d433
debora <deboralaisnm@gmail.com>
2024-07-23 18:09:30 -0300

    feat: Atualizacao  do codigo

--- backend/src/controllers/moodRecordController.js
+++ backend/src/controllers/moodRecordController.js
@@ -14,6 +14,8 @@ class MoodRecordControllers {
     async getEntry(id) {
         try {
             const entry = await Entry.findById(id);
+            // ! -> negação
+		       // ! + entry = se entry não existe/vier
             if (!entry) {
                 return { body: 'Entry not found', success: false, statusCode: 404 };
             }
@@ -24,18 +26,25 @@ class MoodRecordControllers {
     }
 
     async getEntriesByDate(date) {
-        try {
-            const entries = await Entry.find({
-                date: {
-                    $gte: new Date(date + "-01"), // Começo do mês
-                    $lt: new Date(new Date(date + "-01").setMonth(new Date(date + "-01").getMonth() + 1)) // Fim do mês
-                }
-            });
-            return { body: entries, success: true, statusCode: 200 };
-        } catch (error) {
-            return { body: error.message, success: false, statusCode: 500 };
-        }
-    }
+      try {
+          const startDate = new Date(date);
+          startDate.setDate(1); // Define o dia como o primeiro do mês
+  
+          const endDate = new Date(startDate);
+          endDate.setMonth(startDate.getMonth() + 1); // Move para o próximo mês
+          endDate.setDate(0); // Define o dia como o último do mês anterior, efetivamente obtendo o último dia do mês original
+  
+          const entries = await Entry.find({
+              date: {
+                  $gte: startDate, // Começo do mês
+                  $lt: endDate // Fim do mês
+              }
+          });
+          return { body: entries, success: true, statusCode: 200 };
+      } catch (error) {
+          return { body: error.message, success: false, statusCode: 500 };
+      }
+  }
 
     async createEntry(entryData) {
         try {
@@ -71,77 +80,5 @@ class MoodRecordControllers {
     }
 }
 
+
 export default MoodRecordControllers
-
-/*const MoodRecord = require("../models/moodRecordModel.js")
-
-// Função para obter todos os registros de humor
-const getMoodRecords = async (req, res) => {
-  try {
-    const records = await MoodRecord.find({})
-    res.status(200).json(records);
-  } catch (error) {
-    res.status(500).json({ message: error.message })
-  }
-}
-
-// Função para obter um registro específico por ID
-const getMoodRecord = async (req, res) => {
-  try {
-    const { id } = req.params;
-    const record = await MoodRecord.findById(id)
-    if (!record) {
-      return res.status(404).json({ message: "Record not found" })
-    }
-    res.status(200).json(record);
-  } catch (error) {
-    res.status(500).json({ message: error.message })
-  }
-}
-
-// Função para criar um novo registro de humor
-const createMoodRecord = async (req, res) => {
-  try {
-    const record = await MoodRecord.create(req.body)
-    res.status(201).json(record); // Usar status 201 para criação bem-sucedida
-  } catch (error) {
-    res.status(500).json({ message: error.message })
-  }
-}
-
-// Função para atualizar um registro existente
-const updateMoodRecord = async (req, res) => {
-  try {
-    const { id } = req.params;
-    const record = await MoodRecord.findByIdAndUpdate(id, req.body, { new: true }) // Retorna o documento atualizado
-    if (!record) {
-      return res.status(404).json({ message: "Record not found" })
-    }
-    res.status(200).json(record);
-  } catch (error) {
-    res.status(500).json({ message: error.message })
-  }
-}
-
-// Função para deletar um registro
-const deleteMoodRecord = async (req, res) => {
-  try {
-    const { id } = req.params
-    const record = await MoodRecord.findByIdAndDelete(id)
-    if (!record) {
-      return res.status(404).json({ message: "Record not found" })
-    }
-    res.status(200).json({ message: "Record deleted successfully" })
-  } catch (error) {
-    res.status(500).json({ message: error.message })
-  }
-}
-
-module.exports = {
-  getMoodRecords,
-  getMoodRecord,
-  createMoodRecord,
-  updateMoodRecord,
-  deleteMoodRecord,
-}
-*/
--- backend/src/models/moodRecordModel.js
+++ backend/src/models/moodRecordModel.js
@@ -2,38 +2,32 @@ import { Schema, model } from "mongoose";
 
 const moodEnum = ['happy', 'sad', 'angry', 'excited', 'anxious'];
 
-
 // Define o esquema para o modelo de registros de humor
-const MoodRecordSchema = Schema(
+const MoodRecordSchema = new Schema(
   {
     date: {
       type: Date,
       required: [true, "Please enter the date of the entry"],
+      default: Date.now, // Define a data atual como padrão
     },
-
     mood: {
       type: String,
-      required: true,
+      required: [true, "Please select a mood"],
       enum: moodEnum,
     },
-
     stress_level: {
       type: Number,
-      min: 0,
-      max: 10,
-      required: false,
+      min: [0, "Stress level must be between 0 and 10"],
+      max: [10, "Stress level must be between 0 and 10"],
     },
-
     anxiety_level: {
       type: Number,
-      min: 0,
-      max: 10,
-      required: false,
+      min: [0, "Anxiety level must be between 0 and 10"],
+      max: [10, "Anxiety level must be between 0 and 10"],
     },
-
     note: {
       type: String,
-      required: false,
+      trim: true, // Remove espaços em branco no início e no fim
     },
   },
   {
@@ -41,7 +35,6 @@ const MoodRecordSchema = Schema(
   }
 );
 
+const MoodRecord = model("MoodRecord", MoodRecordSchema);
 
-const MoodRecord = model("MoodRecord", MoodRecordSchema)
-
-export default MoodRecord
+export default MoodRecord;
